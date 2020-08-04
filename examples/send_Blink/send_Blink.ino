6b417046d6234ee3b819bb1e7f6b092d246dba49
Albertduplessis <69053108+Albertduplessis@users.noreply.github.com>
2020-08-04 18:16:51 +0200

    Update send_Blink.ino

--- examples/send_Blink/send_Blink.ino
+++ examples/send_Blink/send_Blink.ino
@@ -4,9 +4,9 @@
 
 /*SAMD core*/
 #ifdef ARDUINO_SAMD_VARIANT_COMPLIANCE
-    #define SERIAL SerialUSB
+#define SERIAL SerialUSB
 #else
-    #define SERIAL Serial
+#define SERIAL Serial
 #endif
 
 // the cs pin of the version after v1.1 is default to D9
@@ -18,23 +18,23 @@ const int ledLOW     = 0;
 MCP_CAN CAN(SPI_CS_PIN);                                    // Set CS pin
 
 void setup() {
-    SERIAL.begin(115200);
+  SERIAL.begin(115200);
 
-    while (CAN_OK != CAN.begin(CAN_500KBPS)) {            // init can bus : baudrate = 500k
-        SERIAL.println("CAN BUS Shield init fail");
-        SERIAL.println(" Init CAN BUS Shield again");
-        delay(100);
-    }
-    SERIAL.println("CAN BUS Shield init ok!");
+  while (CAN_OK != CAN.begin(CAN_500KBPS)) {            // init can bus : baudrate = 500k
+    SERIAL.println("CAN BUS Shield init fail");
+    SERIAL.println(" Init CAN BUS Shield again");
+    delay(100);
+  }
+  SERIAL.println("CAN BUS Shield init ok!");
 }
 
 unsigned char stmp[8] = {ledHIGH, 1, 2, 3, ledLOW, 5, 6, 7};
 
 void loop() {
-    SERIAL.println("In loop");
-    // send data:  id = 0x70, standard frame, data len = 8, stmp: data buf
-    CAN.sendMsgBuf(0x70, 0, 8, stmp);
-    delay(1000);                       // send data once per second
+  SERIAL.println("In loop");
+  // send data:  id = 0x70, standard frame, data len = 8, stmp: data buf
+  CAN.sendMsgBuf(0x70, 0, 8, stmp);
+  delay(1000);                       // send data once per second
 }
 
 /*********************************************************************************************************
